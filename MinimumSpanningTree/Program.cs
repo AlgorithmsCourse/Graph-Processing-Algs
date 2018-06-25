f6e80094e1f228ecb7caa30e0a0bcafdde8e0f03
Beth Hart <errordrivendev@gmail.com>
2018-06-25 11:44:33 -0700

    Add project files.

---
+++ MinimumSpanningTree/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MinimumSpanningTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
